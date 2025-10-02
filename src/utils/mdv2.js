6e2eb4ef16378c238d655e53b6b2bdbad2908b5c
javohir <javoxir8177@gmail.com>
2025-10-02 13:26:29 +0500

    maps

--- src/utils/mdv2.js
+++
@@ -1,9 +0,0 @@
-function escMdV2(s = '') {
-    return String(s).replace(/[_*[\](){}`~>#+\-=|.!]/g, '\\$&');
-}
-
-function dashify(s = '') {
-    return String(s).replace(/\s-\s/g, ' – ');
-}
-
-module.exports = { escMdV2, dashify };
